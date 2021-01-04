5ce67630bada73aabbce937b7a5dfee015358959
Klear <kingklear@gmail.com>
2021-01-04 02:32:18 +0100

    Initial commit

---
+++ main/cube.go
@@ -0,0 +1,132 @@
+embedded_components {
+  id: "face_2"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_2.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.5
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "face_1"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_1.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: -0.5
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: -0.70710677
+    z: 0.0
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "face_3"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_3.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.5
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.70710677
+    z: 0.0
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "face_4"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_4.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -0.5
+  }
+  rotation {
+    x: 0.0
+    y: 1.0
+    z: 0.0
+    w: 6.123234E-17
+  }
+}
+embedded_components {
+  id: "face_6"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_6.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: -0.5
+    z: 0.0
+  }
+  rotation {
+    x: 0.70710677
+    y: 0.0
+    z: 0.0
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "face_5"
+  type: "model"
+  data: "mesh: \"/builtins/assets/meshes/quad.dae\"\n"
+  "material: \"/render/cube_5.material\"\n"
+  "skeleton: \"\"\n"
+  "animations: \"\"\n"
+  "default_animation: \"\"\n"
+  "name: \"unnamed\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.5
+    z: 0.0
+  }
+  rotation {
+    x: -0.70710677
+    y: 0.0
+    z: 0.0
+    w: 0.70710677
+  }
+}
