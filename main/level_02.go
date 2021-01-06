563e4d7af39ea764fcbf690b52ea057a54f6f305
K-Klear <41452954+K-Klear@users.noreply.github.com>
2021-01-06 18:52:32 +0100

    Work at work
    Loader script can now load a level of arbitrary size and it works.
    
    Player start location isn't defined just yet.

---
+++ main/level_02.go
@@ -0,0 +1,41 @@
+components {
+  id: "layout"
+  component: "/main/level_02.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"/main/level_02.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"solid\"\n"
+  "mask: \"player\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
